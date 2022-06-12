66c520ffb4a2153ae41a76fe0758030a20c9ffaf
terwer <youweics@163.com>
2022-06-13 02:50:32 +0800

    fix:test env

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index() {
-
+  console.log("WORDPRESS_API_URL from env=>", process.env.WORDPRESS_API_URL)
   return (
     <Layout>
       <Head>
